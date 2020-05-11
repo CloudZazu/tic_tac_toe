f4ffc89b9fd2695d5948893c28c5c90c5d843679
CloudZazu <>
2020-05-10 20:53:00 -0700

    Use loops for tic_tac_toe matrix

--- app/src/index.js
+++ app/src/index.js
@@ -14,30 +14,33 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
     <Square 
+    key = {i}
     value={this.props.squares[i]}
     onClick={() => this.props.onClick(i)}
      />
     );
   }
 
+  getLayout() {
+    let NUM_ROWS = 3;
+    let NUM_COLS = 3;
+
+    var rows = [];
+    for (let row=0; row<NUM_ROWS; row++) {
+      let curRow = [];
+      for (let col=0; col<NUM_COLS; col++) {
+        let idx = row * NUM_COLS + col;
+        curRow.push(this.renderSquare(idx));
+      }
+      rows.push(<div key={row} className='board-row'>{curRow}</div>);
+    }
+    return rows;
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.getLayout()}
       </div>
     );
   }
